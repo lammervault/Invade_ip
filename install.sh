26bc841efa5ac0c29ba48dc1056ae083b36751b2
Sidney PEPO (aka sysb1n) <sidneypepo@pm.me>
2021-03-08 22:02:05 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 function explicação {
-   apt install python -y
+   apt install python3 -y
+   apt install python3-pip -y
    if [ $? -eq 0 ]; then
        echo -e "[\e[32m√\e[0m] Instalado Com sucesso!\n\n"
        echo -e "A ferramenta fara uma varredura de ips próximos ira\nInvadir o mais proximo\n"
        read -p "Press enter para continuar"
        clear; python invade.py
    else
-       echo -e "[\e[1;31mX\e[0m] ERRO CONECTION NOT FOUND!!'
+       echo -e "[\e[1;31mX\e[0m] ERRO CONECTION NOT FOUND!!"
        sleep 4
    fi
 }
