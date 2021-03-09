ab5c26787244e757d59ad73a823b2c0b96c1d6a2
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-03-08 21:51:32 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+function explicação {
+   apt install python -y
+   if [ $? -eq 0 ]; then
+       echo -e "[\e[32m√\e[0m] Instalado Com sucesso!\n\n"
+       read -p "Press enter para continuar"
+       clear; python invade.py
+   else
+       echo -e "[\e[1;31mX\e[0m] ERRO CONECTION NOT FOUND!!'
+       sleep 4
+   fi
+}
+
+explicação
